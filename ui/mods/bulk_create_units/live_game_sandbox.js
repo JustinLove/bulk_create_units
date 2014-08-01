c3697cbed9c692c2a75533ede640a94523ae10f7
Justin Love <git@JustinLove.name>
2014-07-31 23:06:02 -0500

    log scale

--- ui/mods/bulk_create_units/live_game_sandbox.js
+++ ui/mods/bulk_create_units/live_game_sandbox.js
@@ -1,6 +1,10 @@
 (function() {
   model.bulkPasteCount = ko.observable(10)
-  model.bulkPasteOptions = ko.observable({min: 1, max: 100, step: 1})
+  model.bulkPasteOptions = ko.observable({min: 0.1, max: 3, step: 0.01})
+  model.bulkPasteSlider = ko.observable(1)
+  model.bulkPasteSlider.subscribe(function(n) {
+    model.bulkPasteCount(Math.round(Math.pow(10, n)))
+  })
 
   var controls = 'coui://ui/mods/bulk_create_units/bulk_paste_count.html'
   var addControls = function() {
