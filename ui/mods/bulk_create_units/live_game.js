cda0655a625afb0be9c54b3df861698f9f02d167
Justin Love <git@JustinLove.name>
2015-09-22 21:34:56 -0500

    replace engine call with raycast and create unit

--- ui/mods/bulk_create_units/live_game.js
+++ ui/mods/bulk_create_units/live_game.js
@@ -1,11 +1,60 @@
 define([], function() {
+  // Pointer tracking
+  var mouseX = 0
+  var mouseY = 0
+  var hdeck = model.holodeck
+  var mousetrack = function(e) {
+    mouseX = e.offsetX
+    mouseY = e.offsetY
+    hdeck = $(this).data('holodeck')
+  }
+
+  if (model.cheatAllowCreateUnit()) {
+    $('body').on('mousemove', 'holodeck', mousetrack)
+  }
+
+  model.cheatAllowCreateUnit.subscribe(function(value) {
+    if (value) {
+      $('body').on('mousemove', 'holodeck', mousetrack)
+    } else {
+      $('body').off('mousemove', 'holodeck', mousetrack)
+    }
+  })
+
+  armyIndex = ko.computed(function() {
+    return model.playerControlFlags().indexOf(true)
+  })
+
   // keep puppetmaster's special effects if it's present
   model.pasteUnits = model.pasteUnits || function(n) {
     if (!model.cheatAllowCreateUnit()) return
     if (n < 1) return
+    if (armyIndex() == -1) return
+    var army_id = model.players()[armyIndex()].id
+
+    var scale = api.settings.getSynchronous('ui', 'ui_scale') || 1.0;
+
+    var x = Math.floor(mouseX * scale);
+    var y = Math.floor(mouseY * scale);
+
+    hdeck.raycast(x, y).then(function(result) {
+      var drop = {
+        army: army_id,
+        what: selectedUnit(),
+        planet: result.planet,
+        location: result.pos,
+      }
+      pasteUnits3D(n, drop)
+    })
+  }
+
+  var pasteUnits3D = function(n, config) {
+    if (!model.cheatAllowCreateUnit()) return
+    if (n < 1) return
+    if (!config.what || config.what == '') return
 
     for (var i = 0;i < n;i++) {
-      engineCall("unit.debug.paste")
+      model.send_message('create_unit', config)
     }
   }
 
